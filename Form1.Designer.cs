8df2fc965be2b34b87693ad858908a5cc00e0876
Radax983 <55852542+Radax983@users.noreply.github.com>
2024-02-07 11:48:51 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace CRUDApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblStudentName = new System.Windows.Forms.Label();
+            this.lblStudentCourse = new System.Windows.Forms.Label();
+            this.lblStudentFee = new System.Windows.Forms.Label();
+            this.txtStudentName = new System.Windows.Forms.TextBox();
+            this.txtStudentCourse = new System.Windows.Forms.TextBox();
+            this.txtStudentFee = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnView = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(284, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CRUDApp C# ";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtStudentFee);
+            this.groupBox1.Controls.Add(this.txtStudentCourse);
+            this.groupBox1.Controls.Add(this.txtStudentName);
+            this.groupBox1.Controls.Add(this.lblStudentFee);
+            this.groupBox1.Controls.Add(this.lblStudentCourse);
+            this.groupBox1.Controls.Add(this.lblStudentName);
+            this.groupBox1.Location = new System.Drawing.Point(25, 119);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(372, 228);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Student";
+            // 
+            // lblStudentName
+            // 
+            this.lblStudentName.AutoSize = true;
+            this.lblStudentName.Location = new System.Drawing.Point(20, 50);
+            this.lblStudentName.Name = "lblStudentName";
+            this.lblStudentName.Size = new System.Drawing.Size(47, 16);
+            this.lblStudentName.TabIndex = 0;
+            this.lblStudentName.Text = "Name:";
+            // 
+            // lblStudentCourse
+            // 
+            this.lblStudentCourse.AutoSize = true;
+            this.lblStudentCourse.Location = new System.Drawing.Point(20, 100);
+            this.lblStudentCourse.Name = "lblStudentCourse";
+            this.lblStudentCourse.Size = new System.Drawing.Size(53, 16);
+            this.lblStudentCourse.TabIndex = 0;
+            this.lblStudentCourse.Text = "Course:";
+            // 
+            // lblStudentFee
+            // 
+            this.lblStudentFee.AutoSize = true;
+            this.lblStudentFee.Location = new System.Drawing.Point(20, 157);
+            this.lblStudentFee.Name = "lblStudentFee";
+            this.lblStudentFee.Size = new System.Drawing.Size(34, 16);
+            this.lblStudentFee.TabIndex = 0;
+            this.lblStudentFee.Text = "Fee:";
+            // 
+            // txtStudentName
+            // 
+            this.txtStudentName.Location = new System.Drawing.Point(122, 43);
+            this.txtStudentName.Name = "txtStudentName";
+            this.txtStudentName.Size = new System.Drawing.Size(211, 22);
+            this.txtStudentName.TabIndex = 1;
+            // 
+            // txtStudentCourse
+            // 
+            this.txtStudentCourse.Location = new System.Drawing.Point(122, 94);
+            this.txtStudentCourse.Name = "txtStudentCourse";
+            this.txtStudentCourse.Size = new System.Drawing.Size(211, 22);
+            this.txtStudentCourse.TabIndex = 1;
+            // 
+            // txtStudentFee
+            // 
+            this.txtStudentFee.Location = new System.Drawing.Point(122, 151);
+            this.txtStudentFee.Name = "txtStudentFee";
+            this.txtStudentFee.Size = new System.Drawing.Size(211, 22);
+            this.txtStudentFee.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(25, 376);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(136, 45);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnView
+            // 
+            this.btnView.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnView.Location = new System.Drawing.Point(261, 376);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(136, 45);
+            this.btnView.TabIndex = 2;
+            this.btnView.Text = "View";
+            this.btnView.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(414, 119);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(365, 302);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnView);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "CRUDAPP OOP C#";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblStudentFee;
+        private System.Windows.Forms.Label lblStudentCourse;
+        private System.Windows.Forms.Label lblStudentName;
+        private System.Windows.Forms.TextBox txtStudentFee;
+        private System.Windows.Forms.TextBox txtStudentCourse;
+        private System.Windows.Forms.TextBox txtStudentName;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnView;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDApp3
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Course { get; set; }
+        public string Fee { get; set; }
+
+
+    }
+}
